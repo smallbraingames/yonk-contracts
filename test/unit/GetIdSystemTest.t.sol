337fb37ed455cd94df4a7788d56933a559eff963
smallbraingames <smallbraindev@gmail.com>
2023-12-16 15:56:54 -0800

    feat: test that ids increment

--- test/unit/GetIdSystemTest.t.sol
+++ test/unit/GetIdSystemTest.t.sol
@@ -2,8 +2,16 @@
 pragma solidity >=0.8.21;
 
 import { YellTest } from "../YellTest.t.sol";
+import { Id } from "codegen/index.sol";
 
 contract GetIdSystemTest is YellTest {
+  function test_IncrementsId() public {
+    assertEq(Id.get(), 0);
+    world.register({ devicePublicKeyX: 0, devicePublicKeyY: 0 });
+    assertEq(Id.get(), 1);
+    world.register({ devicePublicKeyX: 0, devicePublicKeyY: 0 });
+    assertEq(Id.get(), 2);
+  }
 
   function test_RevertsWhen_GetIdNotCalledFromAllowedSystems() public {
     vm.expectRevert();
