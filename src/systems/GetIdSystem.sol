66fd18f03c379d3840b09d183e72a041b2dfe8f1
smallbraingames <smallbraindev@gmail.com>
2023-12-16 15:43:58 -0800

    feat: get id system

---
+++ src/systems/GetIdSystem.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.21;
+
+import { System } from "@latticexyz/world/src/System.sol";
+
+import { Id } from "codegen/index.sol";
+
+contract GetIdSystem is System {
+    function getId() public returns (uint64) {
+        uint64 id = Id.get() + 1;
+        Id.set({value: id});
+        return id;
+    }
+}
