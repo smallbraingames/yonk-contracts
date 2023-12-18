2961c2af0fe24f3eee3268613be5d3d6d069ef8a
smallbraingames <smallbraindev@gmail.com>
2023-12-18 10:50:20 -0800

    feat: claim system

---
+++ src/libraries/LibClaim.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.21;
+
+import { P256 } from "p256-verifier/P256.sol";
+
+library LibClaim {
+    function isAlive(uint256 startTimestamp, uint256 lifeSeconds) internal view returns (bool) {
+        return block.timestamp < startTimestamp + lifeSeconds;
+    }
+
+    function isValidSignature(
+        bytes32 dataCommitment,
+        uint256 r,
+        uint256 s,
+        uint256 x,
+        uint256 y
+    )
+        internal
+        view
+        returns (bool)
+    {
+        return P256.verifySignature({ message_hash: dataCommitment, r: r, s: s, x: x, y: y });
+    }
+
+    function getYellAmount(
+        uint256 startValue,
+        uint256 endValue,
+        uint256 startTimestamp,
+        uint256 lifeSeconds
+    )
+        internal
+        view
+        returns (uint256)
+    {
+        uint256 timePassed = block.timestamp - startTimestamp;
+        uint256 valueLost = (startValue - endValue) * timePassed / lifeSeconds;
+        uint256 valueLeft = startValue - valueLost;
+        return valueLeft;
+    }
+}
--- src/systems/ClaimSystem.sol
+++ src/systems/ClaimSystem.sol
@@ -2,5 +2,72 @@
 pragma solidity >=0.8.21;
 
 import { System } from "@latticexyz/world/src/System.sol";
+import { RegisteredAddress, Registration, RegistrationData, Yell, YellData } from "codegen/index.sol";
 
-contract ClaimSystem is System { }
+import { LibClaim } from "libraries/LibClaim.sol";
+import { LibRegister } from "libraries/LibRegister.sol";
+
+contract ClaimSystem is System {
+    error AlreadyClaimed();
+    error ClaimerNotRegistered();
+    error IncorrectData();
+    error InvalidSignature();
+    error NotYourYell();
+    error YellExpired();
+
+    function claim(uint256 dataCommitmentPreimage, uint256 signatureR, uint256 signatureS, uint64 yellId) public {
+        YellData memory yellData = Yell.get({ id: yellId });
+
+        if (yellData.claimed) {
+            revert AlreadyClaimed();
+        }
+
+        address toAddress = _msgSender();
+        if (!LibRegister.isRegistered({ accountAddress: toAddress })) {
+            revert ClaimerNotRegistered();
+        }
+
+        uint64 toId = LibRegister.getAddressId({ accountAddress: toAddress });
+        if (yellData.to != toId) {
+            revert NotYourYell();
+        }
+
+        bytes32 dataCommitment = keccak256(abi.encodePacked(dataCommitmentPreimage));
+        if (yellData.dataCommitment != dataCommitment) {
+            revert IncorrectData();
+        }
+
+        if (!LibClaim.isAlive({ startTimestamp: yellData.startTimestamp, lifeSeconds: yellData.lifeSeconds })) {
+            revert YellExpired();
+        }
+
+        RegistrationData memory registrationData = Registration.get({ id: toId });
+        if (
+            !LibClaim.isValidSignature({
+                dataCommitment: dataCommitment,
+                r: signatureR,
+                s: signatureS,
+                x: registrationData.devicePublicKeyX,
+                y: registrationData.devicePublicKeyY
+            })
+        ) {
+            revert InvalidSignature();
+        }
+
+        Yell.setClaimed({ id: yellId, claimed: true });
+
+        uint256 yellAmount = LibClaim.getYellAmount({
+            startValue: yellData.startValue,
+            endValue: yellData.endValue,
+            startTimestamp: yellData.startTimestamp,
+            lifeSeconds: yellData.lifeSeconds
+        });
+
+        uint256 returnAmount = yellData.startValue - yellAmount;
+        address fromAddress = RegisteredAddress.get({ id: yellData.from });
+        payable(toAddress).transfer(yellAmount);
+        if (returnAmount > 0) {
+            payable(fromAddress).transfer(returnAmount);
+        }
+    }
+}
